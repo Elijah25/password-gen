862a42d1392af314f44be00e3fd61c4cf3529c3e
Elijah <elijaherap3@gmail.com>
2020-03-29 16:12:09 -0700

    password criteria confirmations added

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,42 @@
 // Assignment code here
 var charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*?"
 
+// determine how many characters will be in the generated password
 var passwordLength = function() {
   var charNumber = window.prompt("How many characters would you like your password to have?") 
+    // if the answer is an integer not between 8 or 128, run the passwordLenght function again until an acceptable answer is entered
     if (charNumber < 8 || charNumber > 128) {
       window.alert("Please pick a number between 8 and 128.");
       passwordLength ();
     }
 }
 
-var charInfo = function() {
-  var charSpecial = window.confirm() {
-    
-  }
-  var charNumbers = window.confirm 
-  var charCaps = window.confirm 
-}
-
-if (charSpecial) {
-  charSet = 
+// determine if the password should have captial letters, special characters, and/or numbers
+var passwordInfo = function() {
+  var charSpecial = window.confirm("Would You like your password to have special characters?") 
+    if (!charSpecial) {
+      charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    }
+    else charSet = charSet
+  var charNumbers = window.confirm ("Would you like your password to have numbers?")
+    if (!charNumbers) {
+      charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?"
+    }
+    else if ((!charNumbers) && (!charSpecial)){
+      charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+    else charSet = charSet
+  var charCaps = window.confirm ("Would you like your password to have capital letters?") 
+    if (!charCaps) {
+      charSet = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*?"
+    }
+    else if ((!charCaps) && (!charSpecial)) {
+      charSet = "abcdefghijklmnopqrstuvwxyz1234567890"
+    }
+    else if ((!charCaps) && (!charNumbers)) {
+      charSet = "abcdefghijklmnopqrstuvwxyz!@#$%^&*?"
+    }
+    else charSet = charSet
 }
 
 // for loop that loops as many times as there are characters in the password as determined by passwordLength
@@ -26,6 +44,7 @@ if (charSpecial) {
 
 var generatePassword = function () {
   passwordLength();
+  passwordInfo();
 }
 
 // Get references to the #generate element
